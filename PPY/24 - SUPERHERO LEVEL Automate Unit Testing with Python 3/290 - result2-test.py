367fa8f1b8dfdf55fdfa2005fcbdd796dc9d3379
Zo3rb <ali.f90@outlook.com>
2026-08-12 18:48:20 +0300

    Deleted PPY Folder | :rocket:

--- PPY/24 - SUPERHERO LEVEL Automate Unit Testing with Python 3/290 - result2-test.py
+++
@@ -1,4 +0,0 @@
-from basic import my_func
-
-def test_result2():
-    assert my_func(1, 2, 3) == 6
--- PPY/24 - SUPERHERO LEVEL Automate Unit Testing with Python 3/290 - test-result1.py
+++
@@ -1,4 +0,0 @@
-from basic import my_func
-
-def test_result1():
-    assert my_func(1, 2, 3) == 5
